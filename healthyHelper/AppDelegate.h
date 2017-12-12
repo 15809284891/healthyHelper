996dc9dfb278cf09d7c0c5f9493b22875e805d29
15809284891 <1030872851@qq.com>
2017-12-12 09:47:07 +0800

    Initial Commit

---
+++ healthyHelper/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  healthyHelper
+//
+//  Created by snow on 2017/12/12.
+//  Copyright © 2017年 snow. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ healthyHelper/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  healthyHelper
+//
+//  Created by snow on 2017/12/12.
+//  Copyright © 2017年 snow. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
